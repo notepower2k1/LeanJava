6fed9965490b31df389364823e128487056419f9
notepower2k1 <83808551+notepower2k1@users.noreply.github.com>
2021-09-27 19:03:41 +0700

    Add files via upload

---
+++ BaiThucHanh3/Bai1/MainGUI.java
@@ -0,0 +1,11 @@
+package Bai1;
+
+public class MainGUI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new SimpleMath();
+		
+	}
+
+}
---
+++ BaiThucHanh3/Bai2_BMI/Program.java
@@ -0,0 +1,10 @@
+package Bai2_BMI;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new BMICalculator();
+	}
+
+}
